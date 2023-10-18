4db9bb80918a1fa7e0c3831996c44757d62f4e3a
MedSouleim <med.souleim1997@gmail.com>
2023-10-18 16:55:01 +0100

    commit&push

---
+++ Programming Basics/Week 1/Day 1/Optional/code.js
@@ -0,0 +1,7 @@
+//this variable takes the value 0 and will be used for conting
+var likeCount = 0;
+//this function will increase the value of the variable
+function increaseLikes() {
+    // this operation will actualy increase the variable
+    likeCount = likeCount + 1;
+}
