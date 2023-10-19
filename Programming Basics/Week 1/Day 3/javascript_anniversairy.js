6fe383b26ebd334594450e918cde60e3f3b8a188
MedSouleim <med.souleim1997@gmail.com>
2023-10-19 10:16:30 +0100

    commit and push

---
+++ Programming Basics/Week 1/Day 3/javascript_anniversairy.js
@@ -0,0 +1,35 @@
+var star=0;
+function js(){
+    for(var year=1996;year<=2023;year++){ 
+            if(year==2023){
+            console.log(year+"happy 27 year js");
+        }else if(star==10){
+            console.log(year+'happy decade days js');
+            star=0;
+        }else{
+            console.log(year+'happy new year js');
+        }star+=1;
+}
+}
+js();
+function js(){
+    for(var year=1996;year<=2023;year++){
+        if(year==2023){
+            console.log('happy 27 year js');
+        }else if(year%10==5){
+            console.log('happy decade years');
+        }else{
+            console.log('awesome year js');
+        }
+    }
+}
+js();
+
+
+
+
+
+
+
+
+
