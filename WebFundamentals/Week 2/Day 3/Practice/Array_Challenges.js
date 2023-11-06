37a675f3f056cfc6170a5a1a65a59ef604e2f98c
MedSouleim <med.souleim1997@gmail.com>
2023-11-06 20:52:28 +0100

    commit and push

---
+++ WebFundamentals/Week 2/Day 3/Practice/Array_Challenges.js
@@ -0,0 +1,69 @@
+function alwaysHungry(arr) {
+    var hasFood = false;
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === "food") {
+            console.log("yummy");
+            hasFood = true;
+        }
+    }
+
+    if (!hasFood) {
+        console.log("I'm hungry");
+    }
+}
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// // this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    // your code here
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > cutoff) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
+function betterThanAverage(arr) {
+    var sum = 0;
+    // calculate the average
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    sum = sum / arr.length;
+    var count = 0
+    // count how many values are greated than the average
+    for (var j = 0; j < arr.length; j++) {
+        if (arr[j] > sum) {
+            count += 1
+        }
+    }
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
+
+function reverse(arr) {
+    // your code here
+    arr.reverse();
+    return arr;
+}
+
+var result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result); // we expect back ["e", "d", "c", "b", "a"]
+function fibonacciArray(n) {
+    // the [0, 1] are the starting values of the array to calculate the rest from
+    var fibArr = [0, 1];
+    // your code here
+    for (var i = 2; i < n; i++) {
+        var nextFib = fibArr[i - 1] + fibArr[i - 2];
+        fibArr.push(nextFib);
+    }
+    return fibArr;
+}
+var result = fibonacciArray(20);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
