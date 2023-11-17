4b6f3cf20cb4b26c66c2a14c233b22b40c2e9335
MedSouleim <147490372+MedSouleim@users.noreply.github.com>
2023-11-17 16:53:14 +0100

    commit and push

--- Python/Week 1/Day 2/Core/userbankaccount.py
+++ Python/Week 1/Day 2/Core/userbankaccount.py
@@ -38,8 +38,6 @@ account2 = BankAccount(balance = 2000 , int_rate = 0.5)
 account.deposit(200).deposit(50).deposit(300).withdraw(1500).yield_interest().display_account_info()
 account2.deposit(50).withdraw(100).withdraw(400).withdraw(30).withdraw(10).yield_interest().display_account_info()
 BankAccount.allAccount()
-
-
 class User:
     def __init__(self, name, email):
         self.name = name
@@ -60,5 +58,61 @@ class User:
         # self.account.yield_interest()
         print(f"User balance is :{self.account.balance}")
         # return(self)
-account3 = User(name="Itachi",email="itachi@gmail.com")
-account3.make_deposit(100).make_withdraw(50).make_display_account_info()
+user1 = {
+    "first_name": "Itachi",
+    "last_name": "Uchiha",
+    "email": "Itachi@example.com",
+    "age": 30,
+    "is_rewards_member": False,
+    "gold_card_points": 0
+}
+user2 = {
+    "first_name": "Sasuke",
+    "last_name": "Uchiha",
+    "email": "Sasuke@example.com",
+    "age": 28,
+    "is_rewards_member": False,
+    "gold_card_points": 0
+}
+user3 = {
+    "first_name": "Naruto",
+    "last_name": "Uzumaki",
+    "email": "Naruto@example.com",
+    "age": 32,
+    "is_rewards_member": False,
+    "gold_card_points": 0
+}
+def display_info(user):
+    print("First Name:", user["first_name"])
+    print("Last Name:", user["last_name"])
+    print("Email:", user["email"])
+    print("Age:", user["age"])
+    print("Rewards Member:", user["is_rewards_member"])
+    print("Gold Card Points:", user["gold_card_points"])
+    print("")
+def enroll(user):
+    if not user["is_rewards_member"]:
+        user["is_rewards_member"] = True
+        user["gold_card_points"] = 200
+        print("Enrolled successfully!")
+    else:
+        print("Already a rewards member!")
+def spend_points(user, amount):
+    if amount <= user["gold_card_points"]:
+        user["gold_card_points"] -= amount
+        print(f"{amount} points spent. Remaining points: {user['gold_card_points']}")
+    else:
+        print("Insufficient points to spend.")
+display_info(user1)
+display_info(user2)
+display_info(user3)
+spend_points(user1, 50)
+enroll(user2)
+spend_points(user2, 80)
+display_info(user1)
+display_info(user2)
+enroll(user1)
+spend_points(user3, 40)
+display_info(user1)
+display_info(user2)
+display_info(user3)
