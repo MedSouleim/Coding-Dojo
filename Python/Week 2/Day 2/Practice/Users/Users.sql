46c6b159e018eeb2045d4a29506a25b96f977c64
MedSouleim <147490372+MedSouleim@users.noreply.github.com>
2023-11-21 20:01:06 +0100

    commit and push

---
+++ Python/Week 2/Day 2/Practice/Users/Users.sql
@@ -0,0 +1,44 @@
+
+1
+select * FROM users;
+
+2
+/*INSERT INTO users (first_name,last_name,email) 
+VALUES('Med Souleim', 'BENSAID','med.souleim1997@gmail.com'),
+	  ('Jasser','Boulila','jasser@yahoo.com'),
+      ('Anis','Rouis','anis@hotmail.com');*/
+      
+3      
+/*SET SQL_SAFE_UPDATES = 0;
+DELETE from  users WHERE id IN (7,8,9);*/
+
+4
+
+
+-- SELECT * FROM users where email = 'med.souleim1997@gmail.com' ;
+
+5
+-- select * from users where id = 3 ; 
+
+
+6
+/*UPDATE users
+SET last_name = 'Pancakes' WHERE id = 3;*/
+
+7
+
+-- DELETE from  users WHERE id = 2;
+
+8
+/*SELECT *
+FROM users
+ORDER BY first_name;*/
+
+9
+
+/*SELECT *
+FROM users
+ORDER BY first_name DESC;*/
+
+
+
