97436533e1887845fcd81afddf0d5aceeaa6b240
MedSouleim <147490372+MedSouleim@users.noreply.github.com>
2023-12-19 15:32:34 +0100

    commit and push

--- MERNStack/week_1/Day_2/practice/itsTimeToDuel.js
+++ MERNStack/week_1/Day_2/practice/itsTimeToDuel.js
@@ -1,43 +1,42 @@
 class Card {
-    constructor(name, cost) {
-        this.name = name;
-        this.cost = cost;
+    constructor (name , cost){
+        this.name = name ; 
+        this.cost = cost ;
     }
 }
-class Unit extends Card {
-    constructor(name, cost, power, res) {
-        super(name, cost);
-        this.power = power;
-        this.res = res;
+class Unit extends Card{
+    constructor (name , cost , power , res){
+        super (name , cost);
+        this.power = power ; 
+        this.resilience = this.resilience ;
     }
-    attack(target) {
-        if (target instanceof Unit) {
-            target.res -= this.attack;
-            return target
+    attack(target){
+        if (target instanceof Unit){
+            target.resilience -= this.attack ;
         }
-        else {
-            throw new Error("Target must be a unit!");
+        else{
+            throw new ERROR('Target must be a unit!');
         }
     }
 }
-class Effects extends Card {
-    constructor(name, cost, text, stat, magnitude) {
-        super(name, cost);
-        this.text = text;
-        this.stat = stat;
-        this.magnitude = magnitude;
+class Effect extends Card {
+    constructor (name , cost , text , stat , magnitude) {
+        super (name , cost );
+        this.text = text ; 
+        this.stat = stat ; 
+        this.magnitude = magnitude ; 
     }
-    play(target) {
-        if (target instanceof Unit) {
-            if(this.stat==="resilience"){
-                target.res+=this.magnitude;
+    play(target){
+        if (target instanceof Unit){
+            if (this.stat === 'resilience'){
+                target.resilience += this.magnitude
             }
-            else if(this.stat==="power"){
-                target.power+=this.magnitude;
+            else if (this.stat === 'power'){
+                target.power += this.magnitude
             }
-            return target
-        } else {
-            throw new Error("Target must be a unit!");
+        }
+        else{
+            throw new ERROR('Target must be a unit!');
         }
     }
 }
