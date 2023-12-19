57c0325e24efc5231fbc9f6f99477eec7c59e5d1
MedSouleim <147490372+MedSouleim@users.noreply.github.com>
2023-12-19 15:45:20 +0100

    commit and push

--- MERNStack/week_1/Day_2/practice/itsTimeToDuel.js
+++ MERNStack/week_1/Day_2/practice/itsTimeToDuel.js
@@ -40,3 +40,15 @@ class Effect extends Card {
         }
     }
 }
+const redBeltNinja = new Unit ("Red Belt Ninja",3,3,4);
+const BlackBeltNinja = new Unit ("Black Belt Ninja",4,5,4);
+
+
+const HardAlgo = new Effect ("Hard Algorithm",2,"increase target's resilience by 3","resilience",3);
+const UnhandledPromiseRejection = new Effect ("Unhandled Promise Rejection",1,"	reduce target's resilience by 2","resilience",-2);
+const PairProgramming = new Effect ("Pair Programming",3,"increase target's power by 2","power",2);
+
+UnhandledPromiseRejection.play(redBeltNinja);
+PairProgramming.play(redBeltNinja);
+redBeltNinja.attack(BlackBeltNinja);
+
