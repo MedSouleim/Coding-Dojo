7ad29b11beb1145235926e97b136e42706e371a8
MedSouleim <147490372+MedSouleim@users.noreply.github.com>
2024-01-23 15:38:20 +0100

    First java Program

---
+++ JAVA/Week1/Day1/Practice/FirstJavaProgram/FirstJAVAProgram.java
@@ -0,0 +1,8 @@
+public class FirstJAVAProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Med Souleim Ben Said\n" +
+                "I am 26 years old\n" +
+                "My hometown is Tazarka, Nabeul\n" +
+                "\n");
+    }
+}
