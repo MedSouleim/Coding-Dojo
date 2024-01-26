a83cd51f2ec5ecc9671fc761346d74cd80782ce5
MedSouleim <147490372+MedSouleim@users.noreply.github.com>
2024-01-26 20:57:04 +0100

    commit and push

--- JAVA/Week1/Day1/Practice/Map of the Hashmatique/BandSongList.java
+++ JAVA/Week1/Day1/Practice/Map of the Hashmatique/BandSongList.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class BandSongList {
+import java.util.HashMap;
+import java.util.Map;
+
+public class BandSongList {
+    public static void main(String[] args) {
+        // Create a HashMap to store the track list
+        Map<String, String> trackList = new HashMap<>();
+
+        // Add songs to the trackList
+        trackList.put("Bring Me To Life", "Call my name and save me from the dark...");
+        trackList.put("Going Under", "Now I will tell you what I've done for you...");
+        trackList.put("Hello", "Playground, school-bell rings, again...");
+        trackList.put("Lithium", "Lithium don't want to lock me up inside...");
+
+        // Pull out a song by its track title
+        String songTitleToRetrieve = "Going Under";
+        String lyrics = trackList.get(songTitleToRetrieve);
+        System.out.println("Retrieving song with title '" + songTitleToRetrieve + "':");
+        System.out.println("Track: " + songTitleToRetrieve + "\nLyrics: " + lyrics);
+        System.out.println();
+
+        // Print all track names and lyrics
+        System.out.println("All tracks and lyrics:");
+        for (Map.Entry<String, String> entry : trackList.entrySet()) {
+            System.out.println("Track: " + entry.getKey() + "\nLyrics: " + entry.getValue() + "\n");
+        }
+    }
 }
