2573ffd86c8d7f3c70f545302999217bd334e70e
MedSouleim <147490372+MedSouleim@users.noreply.github.com>
2024-01-29 17:32:10 +0100

    commit and push

--- JAVA/Week1/Day2/Core/BankAccount/BankAccount.java
+++ JAVA/Week1/Day2/Core/BankAccount/BankAccount.java
@@ -1,2 +1,77 @@
-package PACKAGE_NAME;public class BankAccount {
+import java.util.Random;
+
+public class BankAccount {
+    // MEMBER VARIABLES
+    private double checkingBalance;
+    private double savingsBalance;
+    private static int accounts = 0;
+    private static double totalMoney = 0;
+    private final String accountNumber;
+
+    // CONSTRUCTOR
+    public BankAccount(double initialCheckingBalance, double initialSavingsBalance) {
+        this.checkingBalance = initialCheckingBalance;
+        this.savingsBalance = initialSavingsBalance;
+        this.accountNumber = generateAccountNumber();
+        accounts++;
+        totalMoney += initialCheckingBalance + initialSavingsBalance;
+    }
+
+    // GETTERS
+    public double getCheckingBalance() {
+        return checkingBalance;
+    }
+
+    public double getSavingsBalance() {
+        return savingsBalance;
+    }
+
+    public static int getAccounts() {
+        return accounts;
+    }
+
+    public static double getTotalMoney() {
+        return totalMoney;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    // METHODS
+    public void deposit(double amount, String accountType) {
+        if ("checking".equalsIgnoreCase(accountType)) {
+            checkingBalance += amount;
+        } else if ("savings".equalsIgnoreCase(accountType)) {
+            savingsBalance += amount;
+        }
+        totalMoney += amount;
+    }
+
+    public void withdraw(double amount, String accountType) {
+        if ("checking".equalsIgnoreCase(accountType) && checkingBalance >= amount) {
+            checkingBalance -= amount;
+            totalMoney -= amount;
+        } else if ("savings".equalsIgnoreCase(accountType) && savingsBalance >= amount) {
+            savingsBalance -= amount;
+            totalMoney -= amount;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+    }
+
+    public void getBalance() {
+        System.out.println("Checking Balance: " + checkingBalance);
+        System.out.println("Savings Balance: " + savingsBalance);
+    }
+
+    // Private method to generate a random ten-digit account number
+    private String generateAccountNumber() {
+        Random rand = new Random();
+        StringBuilder accountNumberBuilder = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            accountNumberBuilder.append(rand.nextInt(10));
+        }
+        return accountNumberBuilder.toString();
+    }
 }
--- JAVA/Week1/Day2/Core/BankAccount/BankTest.java
+++ JAVA/Week1/Day2/Core/BankAccount/BankTest.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class BankTest {
+public class BankTest {
+    public static void main(String[] args) {
+        // Create 3 bank accounts
+        BankAccount account1 = new BankAccount(1000, 500);
+        BankAccount account2 = new BankAccount(2000, 1000);
+        BankAccount account3 = new BankAccount(500, 200);
+
+        // Deposit Test
+        account1.deposit(200, "checking");
+        account2.deposit(300, "savings");
+        account3.deposit(100, "checking");
+
+        // Display balances after deposits
+        System.out.println("Balances after deposits:");
+        account1.getBalance();
+        account2.getBalance();
+        account3.getBalance();
+        System.out.println("Total Money: " + BankAccount.getTotalMoney());
+
+        // Withdrawal Test
+        account1.withdraw(150, "savings");
+        account2.withdraw(500, "checking");
+        account3.withdraw(300, "savings");
+
+        // Display balances after withdrawals
+        System.out.println("\nBalances after withdrawals:");
+        account1.getBalance();
+        account2.getBalance();
+        account3.getBalance();
+        System.out.println("Total Money: " + BankAccount.getTotalMoney());
+
+        // Static Test
+        System.out.println("\nNumber of accounts: " + BankAccount.getAccounts());
+        System.out.println("Total Money: " + BankAccount.getTotalMoney());
+    }
 }
+
